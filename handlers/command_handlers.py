0d901733548ba4775df47b8c99a02650ca37acd9
AmypDV <amypdv@yandex.ru>
2024-07-01 18:24:55 +0300

    хэндлеры для команд и фразы в лексикон

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -2,7 +2,7 @@ from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from aiogram import Router
 
-from keyboards.keyboard_utils import keyboard_animal
+from keyboards.keyboard_utils import keyboard_startgame
 
 from lexicon.lexicon import LEXICON_RU
 import logging
@@ -12,18 +12,16 @@ logger = logging.getLogger(__name__)
 
 router_command = Router()
 
-# # Этот хэндлер будет срабатывать на команду "/start"
-# @router_command.message(CommandStart())
-# async def process_start_command(message: Message):
-#     logger.info(f'command start. Member {message.from_user.id}')
-#     await message.answer(text=LEXICON_RU().start,
-#                          reply_markup=keyboard_animal)
-#
-#
-#
-#
-# # Этот хэндлер будет срабатывать на команду "/help"
-# @router_command.message(Command(commands='help'))
-# async def process_help_command(message: Message):
-#     logger.info(f'command help. Member {message.from_user.id}')
-#     await message.answer(text=LEXICON_RU().help)
+# Этот хэндлер будет срабатывать на команду "/start"
+@router_command.message(CommandStart())
+async def process_start_command(message: Message):
+    logger.info(f'command start. Member {message.from_user.id}')
+    await message.answer(text=LEXICON_RU().start,
+                         reply_markup=keyboard_startgame)
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@router_command.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    logger.info(f'command help. Member {message.from_user.id}')
+    await message.answer(text=LEXICON_RU().help)
