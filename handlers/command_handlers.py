930b98392a0e0052121f141cdf828fa66b6fc56b
AmypDV <amypdv@yandex.ru>
2024-07-03 11:56:47 +0300

    добавил хэндлеры команд

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -2,7 +2,8 @@ from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from aiogram import Router
 
-from keyboards.keyboard_utils import keyboard_startgame
+from keyboards.keyboard_utils import keyboard_start_game
+from states.states import Player, PLAYERS, write_pickle_db
 
 from lexicon.lexicon import LEXICON_RU
 import logging
@@ -16,12 +17,20 @@ router_command = Router()
 @router_command.message(CommandStart())
 async def process_start_command(message: Message):
     logger.info(f'command start. Member {message.from_user.id}')
+    id = message.from_user.id
+    if id not in PLAYERS:
+        name = message.from_user.first_name
+        PLAYERS[id] = Player(id_player=id,
+                             name_player=name)
+        logger.info(f'add player: name {name}, id {id}')
+        write_pickle_db(PLAYERS)
     await message.answer(text=LEXICON_RU().start,
-                         reply_markup=keyboard_startgame)
+                         reply_markup=keyboard_start_game)
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
 @router_command.message(Command(commands='help'))
 async def process_help_command(message: Message):
     logger.info(f'command help. Member {message.from_user.id}')
-    await message.answer(text=LEXICON_RU().help)
+    await message.answer(text=LEXICON_RU().help,
+                         reply_markup=keyboard_start_game)
