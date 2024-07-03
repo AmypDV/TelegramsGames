e367726c0ca8c2baa1b178f2de90bc92d25e3a44
AmypDV <amypdv@yandex.ru>
2024-07-03 11:57:47 +0300

    добавил хэндлеры игры

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -5,9 +5,9 @@ from aiogram import Router, F
 import logging
 
 from utils.utils import random_unit_of_game, game_ruls
-from keyboards.keyboard_utils import keyboard_game, keyboard_startgame
+from keyboards.keyboard_utils import keyboard_game, keyboard_start_game, keyboard_next_game
 from lexicon.lexicon import LEXICON_RU
-from states.states import Player, PLAYERS
+from states.states import PLAYERS, write_pickle_db
 
 
 
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 router_user = Router()
 
 
-@router_user.message(F.text == 'Начинаем игру "Камень, ножницы, бумага"')
+@router_user.message(F.text.in_({'Сыграем', 'Продолжить игру'}))
 async def start_game(message: Message):
         logger.info(f'start game. Member {message.from_user.id}')
         PLAYERS[message.from_user.id].games_figure = random_unit_of_game()
@@ -32,19 +32,35 @@ async def get_stone_in_game(message: Message):
         await message.answer(text=f'У меня {PLAYERS[message.from_user.id].games_figure}\n'
                                   f'{message.from_user.first_name} у нас ничья!\n'
                                   f'счет {PLAYERS[message.from_user.id].n_wins}:{PLAYERS[message.from_user.id].n_lose}',
-                             reply_markup=keyboard_startgame
+                             reply_markup=keyboard_next_game
                              )
     elif game_ruls(message.text, PLAYERS[message.from_user.id].games_figure):
         PLAYERS[message.from_user.id].n_wins += 1
-        await message.answer(text=f'У меня {PLAYERS[message.from_user.id].games_figure}\n'
-                                  f'{message.from_user.first_name} вы выиграли!\n'
-                                  f'счет {PLAYERS[message.from_user.id].n_wins}:{PLAYERS[message.from_user.id].n_lose}',
-                             reply_markup=keyboard_startgame
+        await message.answer(text=LEXICON_RU.game_win.format(comp_figure=PLAYERS[message.from_user.id].games_figure,
+                                                            first_name=message.from_user.first_name,
+                                                            wins=PLAYERS[message.from_user.id].n_wins,
+                                                            lose=PLAYERS[message.from_user.id].n_lose),
+                             reply_markup=keyboard_next_game
                             )
     elif not game_ruls(message.text, PLAYERS[message.from_user.id].games_figure):
         PLAYERS[message.from_user.id].n_lose += 1
         await message.answer(text=f'У меня {PLAYERS[message.from_user.id].games_figure}\n'
                                   f'{message.from_user.first_name} вы проиграли!\n'
                                   f'счет {PLAYERS[message.from_user.id].n_wins}:{PLAYERS[message.from_user.id].n_lose}',
-                             reply_markup=keyboard_startgame
+                             reply_markup=keyboard_next_game
                             )
+
+@router_user.message(F.text == 'Закончить игру')
+async def start_game(message: Message):
+        logger.info(f'end game. Member {message.from_user.id}')
+        PLAYERS[message.from_user.id].n_games += 1
+        write_pickle_db(PLAYERS)
+        await message.answer(text='До скорых встреч!',
+                             reply_markup=keyboard_start_game
+                             )
+
+@router_user.message(F.text == 'Помощь')
+async def start_game(message: Message):
+        logger.info(f'/help. Member {message.from_user.id}')
+        await message.answer(text=LEXICON_RU.help,
+                             reply_markup=keyboard_start_game)
